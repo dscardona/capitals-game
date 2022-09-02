4b5ee6b33cc2a132ce372c27e4b4ab6dd19d200a
Jeff Schwab <jeff@unbuggy.com>
2022-09-02 09:25:56 -0400

    Reformat code using black --line-length=79

--- classes.py
+++ classes.py
@@ -1,7 +1,8 @@
 from dictionaries import europe
 import random
 
-class GameUser: 
+
+class GameUser:
     def __init__(self, name, level):
         self.name = name
         self.level = level
@@ -15,43 +16,56 @@ class GameUser:
         while True:
             if question_list != []:
                 options = random.sample(options_list, 3)
-                question_country, question_capital = random.choice(question_list)
+                question_country, question_capital = random.choice(
+                    question_list
+                )
 
                 for city in options:
                     if city == question_capital:
                         options.remove(city)
                         options.append(random.choice(options_list))
-                
+
                 options.append(question_capital)
                 random.shuffle(options)
                 options_letters = ["a", "b", "c", "d"]
-                options_menu = (dict(zip(options_letters, options)))
+                options_menu = dict(zip(options_letters, options))
 
-                print(f'\nWhat is the capital of {question_country}?\n')
-            
+                print(f"\nWhat is the capital of {question_country}?\n")
+
                 for letter, option in options_menu.items():
-                    print(f'{letter}) {option}')
-                            
-                print(f'\n(End game at anytime by entering 0)')
+                    print(f"{letter}) {option}")
+
+                print(f"\n(End game at anytime by entering 0)")
                 selection = input("Enter answer: ")
 
                 if selection == "0":
                     return score
-                elif selection == question_capital or (selection in options_menu and options_menu[selection] == question_capital):
+                elif selection == question_capital or (
+                    selection in options_menu
+                    and options_menu[selection] == question_capital
+                ):
                     question_list.remove((question_country, question_capital))
                     score += 1
-                    print(f"You've answered correctly! Score: {score} -- Attempts left: {attempts}")
+                    print(
+                        f"You've answered correctly! Score: {score} -- Attempts left: {attempts}"
+                    )
                     continue
                 else:
                     attempts -= 1
                     if attempts <= 0:
-                        try_again = input("Remaining attempts: 0. Game over. \nTry again? y/n ")
+                        try_again = input(
+                            "Remaining attempts: 0. Game over. \nTry again? y/n "
+                        )
                         if try_again == "n":
                             return score
                         else:
-                            self.level1(0,3)
+                            self.level1(0, 3)
                     else:
-                        print(f"Wrong answer. Remaining attempts: {attempts}. Score: {score}")
+                        print(
+                            f"Wrong answer. Remaining attempts: {attempts}. Score: {score}"
+                        )
             else:
-                print(f"You've completed level 1 with a score of {score} points and {attempts} attempt(s) left")
+                print(
+                    f"You've completed level 1 with a score of {score} points and {attempts} attempt(s) left"
+                )
                 return score
--- functions.py
+++ functions.py
@@ -1,15 +1,16 @@
 def get_num_players():
-    #tried to call funcion recursively when the number of players is invalid. This was causing a type error (function returned None). This is why I opted for an infinite loop instead.
+    # tried to call funcion recursively when the number of players is invalid. This was causing a type error (function returned None). This is why I opted for an infinite loop instead.
     while True:
         num_players = input("Number of players (4 max)? ")
         if num_players.isdigit():
             num_players = int(num_players)
             if num_players <= 4 and num_players > 0:
-                print(f'Configuring game for {num_players} player(s)')
+                print(f"Configuring game for {num_players} player(s)")
                 return num_players
-            print(f'Game can\'t support {num_players} players')
+            print(f"Game can't support {num_players} players")
         else:
             print("Please enter a valid number of players.")
+
 
 def get_name():
     while True:
@@ -21,6 +22,7 @@ def get_name():
         else:
             print(f"Hi, {name}. Welcome to the World Capitals Game!")
             return name
+
 
 def get_level():
     while True:
@@ -34,14 +36,16 @@ def get_level():
         else:
             print("Please enter a valid level number")
 
+
 def multi_player():
     print("multi-player function pending")
     return
 
+
 # def high_score(name, score):
-    # print("high score function pending")
-    # return
+# print("high score function pending")
+# return
 
 # def leaderboard(name, score):
-    # print("leader board function pending")
-    # return
+# print("leader board function pending")
+# return
